c55fbdb65aa6227e138f889698dfd5cbe8c77b24
ps.rafaelfa <ps.rafaelfa@aguiabranca.com.br>
2026-02-16 16:03:58 -0300

    Adiciona Person e PersonController; remove configs e binários
    Foram criados os arquivos PersonController.cs e Person.cs, definindo o controller e o modelo da API. Removidos arquivos de configuração, build, dependências NuGet e binários, deixando apenas o código essencial.

---
+++ RestApi/RestApi.API/Controllers/PersonController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RestApi.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonController : ControllerBase
+    {
+    }
+}
---
+++ RestApi/RestApi.API/Model/Person.cs
@@ -0,0 +1,12 @@
+namespace RestApi.API.Model
+{
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+    }
+}
